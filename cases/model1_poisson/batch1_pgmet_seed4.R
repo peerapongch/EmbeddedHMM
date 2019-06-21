f4a4fad22b9f52315a25cd5618243a00f628374b
Peerapong Chuanromanee <p.chuanromanee@gmail.com>
2019-06-21 15:39:02 +0100

    bug fix

--- cases/model1_poisson/batch1_pgmet_seed4.R
+++ cases/model1_poisson/batch1_pgmet_seed4.R
@@ -5,11 +5,12 @@ source('../../samplers/model1_pgmet.R')
 L <- 250
 N <- 30000
 seed <- 4
+es <- c(0.2,0.8)
 
 s <- system.time(
-  pgmet_out <- pgmetModel1(ssm_poisson,N,L,seed=seed)
+  pgmet_out <- pgmetModel1(ssm_poisson,N,L,es,seed=seed)
 )
 tpi <- s[[3]]/N
-tps <- tpi/4 # 0.30
+tps <- tpi/4 
 
-save(tpi,tps,pgmet_out,file=paste('./data/batch1_pgbs_seed',seed,'.RData',sep=''))
+save(tpi,tps,pgmet_out,file=paste('./data/batch1_pgmet_seed',seed,'.RData',sep=''))
