6bd58754e8f4a6eb9b7f677e26a9a2591d287d68
Peerapong Chuanromanee <p.chuanromanee@gmail.com>
2019-07-17 21:53:04 +0100

    updated gamma

--- cases/model1_poisson/batch2_ehmm_backward_seed1.R
+++ cases/model1_poisson/batch2_ehmm_backward_seed1.R
@@ -2,12 +2,13 @@ setMKLthreads(2)
 load('./data/model1_poisson2.RData')
 load('./data/model1_poisson2_env.RData')
 source('../../samplers/model1_ehmm_backward.R')
-N <- 9000
+N <- 20000
 L <- 50
+elim <- c(0,0.1)
 seed <- 1
 filename <- paste('./data/batch2_ehmm_backward_seed',seed,'.RData',sep='')
 print(filename)
 system.time(
-  ehmm_backward_out <- ehmmModel1_backward(ssm_poisson,N,L,seed=seed) 
+  ehmm_backward_out <- ehmmModel1_backward(ssm_poisson,N,L,seed=seed,elim=elim) 
 )
 save(ehmm_backward_out,file=filename)
